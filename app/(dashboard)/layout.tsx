53cb6864418c39b51c028733ef76e2009768c4ac
codebyshreya <shreyajoshi.learn@gmail.com>
2024-09-07 18:30:16 +0530

    Dashboard

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -6,10 +6,10 @@ const DashboardLayout = ({
 } : {children:React.ReactNode}) => {
     return (
         <div className="h-full relative md:flex">
-            <div className="hidden md:flex md:w-3/12 bg-gray-900 ">
+            <div className="hidden md:flex md:w-1/6 bg-gray-900 ">
             <Sidebar/>
             </div>
-            <div className="md:w-9/12">
+            <div className="md:w-5/6">
             <Navbar />
             {children}
             </div>
--- components/MobileSidebar.tsx
+++ components/MobileSidebar.tsx
@@ -5,12 +5,24 @@ import { Menu } from "lucide-react";
 import Sidebar from "./sidebar";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { useEffect, useState } from "react";
 
 const MobileSidebar = () => {
+  const [isMounted, setIsMounted] = useState(false); // fixed for hidretion error causing button discendent to button while using button inside SheetTrigger
+
+  useEffect(()=> {
+    setIsMounted(true)
+  }, [])
+
+  if (!isMounted) {
+    return null
+  }
   return (
     <Sheet>
-      <SheetTrigger className="md:hidden">
-        <Menu />
+      <SheetTrigger>
+        <Button variant="ghost" size="icon" className="md:hidden">
+          <Menu />
+        </Button>
       </SheetTrigger>
       <SheetContent side="left" className="p-0">
         <Sidebar  />
